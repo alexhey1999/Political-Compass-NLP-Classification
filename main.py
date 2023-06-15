62e2ee311919c27a708b992eda7e33ca07120e94
Alex Hey <64182587+alexhey1999@users.noreply.github.com>
2023-06-15 01:20:21 +0100

    Added argparser function to deal with multiple program paths

--- main.py
+++ main.py
@@ -6,28 +6,33 @@ from Integrations.cato_institute import CatoIntegration
 
 from Integrations.database import Database
 
-def main():
+import argparse
+
+OPTIONS = ["collect", "clear", "nlp"]
+
+# Runs all classes from integration folder
+def integrations_execute(debug = True):
     # Initialize the database
     db = Database()
     
-    # # url, service_name, auth_method, credentials, database_file_name
-    # reddit_api = RedditAPI("www.reddit.com", "Reddit", "No-Auth", None, "database.db")
-    # reddit_api.output_basic_info()
-    # reddit_api.top_posts_subreddit('Libertarian')
+    # url, service_name, auth_method, credentials, database_file_name
+    reddit_api = RedditAPI("www.reddit.com", "Reddit", "No-Auth", None, "database.db")
+    reddit_api.output_basic_info()
+    reddit_api.top_posts_subreddit('Libertarian')
     
-    # occupy_dem_api = OccupyDemocratsAPI("www.occupydemocrats.com", "OccupyDemocrats", "No-Auth", None, "database.db")
-    # occupy_dem_api.output_basic_info()
-    # left_leaning = occupy_dem_api.all_news_reports()
+    occupy_dem_api = OccupyDemocratsAPI("www.occupydemocrats.com", "OccupyDemocrats", "No-Auth", None, "database.db")
+    occupy_dem_api.output_basic_info()
+    left_leaning = occupy_dem_api.all_news_reports()
     
-    # gab_api = GabAPI("www.gab.com", "Gab", "No-Auth", None, "database.db")
-    # gab_api.output_basic_info()
-    # right_leaning = gab_api.all_posts(1874862)
+    gab_api = GabAPI("www.gab.com", "Gab", "No-Auth", None, "database.db")
+    gab_api.output_basic_info()
+    right_leaning = gab_api.all_posts(1874862)
     
     
-    # aj_api = AlJazeeraAPI("www.aljazeera.com", "Al Jazeera", "No-Auth", None, "database.db")
-    # aj_api.output_basic_info()
-    # authoritarian_leaning_opinion = aj_api.all_posts_opinion()
-    # authoritarian_leaning_news = aj_api.all_posts_news(400)
+    aj_api = AlJazeeraAPI("www.aljazeera.com", "Al Jazeera", "No-Auth", None, "database.db")
+    aj_api.output_basic_info()
+    authoritarian_leaning_opinion = aj_api.all_posts_opinion()
+    authoritarian_leaning_news = aj_api.all_posts_news(400)
     
     
     cato_int = CatoIntegration("www.cato.org", "Cato Institute", "No-Auth", None, "database.db")
@@ -36,29 +41,67 @@ def main():
     
     for i in libertarian_leaning:
         db.write_record("Cato Institute", i, "Libertarian", "No")
+
+    print("Writing records...")
     
-    # for i in right_leaning:
-        # db.write_record("Gab - NationalPost", i, "Right", "No")
+    for i in right_leaning:
+        db.write_record("Gab - NationalPost", i, "Right", "No")
     
-    # for i in right_leaning:
-        # db.write_record("Gab - NationalPost", i, "Right", "No")
+    for i in right_leaning:
+        db.write_record("Gab - NationalPost", i, "Right", "No")
         
-    # db.clear_data()
-    # for i in left_leaning:
-    #     db.write_record("OccupyDemocrats", i, "Left", "No")
+    db.clear_data()
+    for i in left_leaning:
+        db.write_record("OccupyDemocrats", i, "Left", "No")
     
-    # for i in reddit_api.top_posts_subreddit('Libertarian'):
-    #     db.write_record("reddit", i, "Libertarian", "No")
+    for i in reddit_api.top_posts_subreddit('Libertarian'):
+        db.write_record("reddit", i, "Libertarian", "No")
     
-    # for i in authoritarian_leaning_opinion:
-    #     db.write_record("Al Jazeera - Opinion", i, "Authoritarian", "No")
+    for i in authoritarian_leaning_opinion:
+        db.write_record("Al Jazeera - Opinion", i, "Authoritarian", "No")
         
-    # for i in authoritarian_leaning_news:
-    #     db.write_record("Al Jazeera - News", i, "Authoritarian", "No")
-        
-    db.commit_database_changes()
+    for i in authoritarian_leaning_news:
+        db.write_record("Al Jazeera - News", i, "Authoritarian", "No")
+    
+    if not debug:
+        db.commit_database_changes()
+    
     db.close_database_connection()
+
+def clear_db(debug):
+    if not debug:
+        print("THIS FUNCTION CLEARS THE DATABASE COMPLETELY! DO NOT USE UNLESS YOU ARE CERTAIN")
+        output = input("TYPE 'CONFIRM' TO START DELETION")
+        if output == "CONFIRM":
+            print("STARTING DELETION")
+            db = Database()
+            db.clear_data()
+            
+        else:
+            print("Exiting...")
+    else:
+        print("Cannot clear db in debug mode...")
+
+# Main Function handles ArgParser and options that can be executed
+def main():
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument('option', choices = OPTIONS, type=str)
+    parser.add_argument('debug', choices = [True, False], type=bool)
+    args = parser.parse_args()
+    
+    # OPTIONS = ["collect", "clear", "nlp"]
+    if args.option == "collect":
+        integrations_execute(args.debug)
+        
+    elif args.option == "clear":
+        
+    
+
 if __name__ == "__main__":
-    print("Start Data Collection")
-    main()
+    
+    
+        
+    
+    
+    # main()
